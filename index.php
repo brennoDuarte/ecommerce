769dbd6af1179be820a99ea774223f83a5aa5712
Brenno Duarte <brenno.gnr@gmail.com>
2018-06-06 07:13:02 -0300

    dados não aparecem

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\
 
 $app = new Slim();
 
@@ -37,7 +38,6 @@ $app->get('/admin/login', function(){
 
 });
 
-
 $app->post('/admin/login', function(){
 
 	User::login($_POST['login'], $_POST['password']);
@@ -51,6 +51,50 @@ $app->get('/admin/logout', function(){
 	header("Location: /admin/login");
 	exit;
 });
+
+$app->get('/admin/users', function(){
+	User::verifyLogin();
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+});
+
+$app->get('/admin/users/create', function(){
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser){
+	User::verifyLogin();
+
+	
+});
+
+$app->get('/admin/users/:iduser', function($iduser){
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	$page->setTpl("users-update");
+});
+
+$app->post('/admin/users/create', function(){
+	User::verifyLogin();
+
+
+});
+
+$app->post('/admin/users/:iduser', function($iduser){
+	User::verifyLogin();
+
+	
+});
+
+
 
 $app->run();
 
