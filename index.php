1480f0fa9465367b359151d9d7645f3179b6f499
Brenno Duarte <brenno.gnr@gmail.com>
2018-06-28 07:29:52 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -28,6 +28,8 @@ $app->get('/admin', function() {
 
 });
 
+//LOGIN
+
 $app->get('/admin/login', function(){
 
 	$page = new PageAdmin([
@@ -46,11 +48,15 @@ $app->post('/admin/login', function(){
 
 });
 
+// LOGOUT
+
 $app->get('/admin/logout', function(){
 	User::logout();
 	header("Location: /admin/login");
 	exit;
 });
+
+// CLASSE USUÁRIO
 
 $app->get('/admin/users', function(){
 	User::verifyLogin();
@@ -61,6 +67,8 @@ $app->get('/admin/users', function(){
 		"users"=>$users
 	));
 });
+
+// CRIAR USUÁRIO
 
 $app->get('/admin/users/create', function(){
 	User::verifyLogin();
@@ -81,6 +89,8 @@ $app->post('/admin/users/create', function(){
 	exit;
 });
 
+// DELETAR USUÁRIO
+
 $app->get('/admin/users/:iduser/delete', function($iduser){
 	User::verifyLogin();
 
@@ -91,6 +101,8 @@ $app->get('/admin/users/:iduser/delete', function($iduser){
 	header("Location: /admin/users");
 	exit;
 });
+
+// ATUALIZAR USUÁRIO
 
 $app->get('/admin/users/:iduser', function($iduser){
 	User::verifyLogin();
@@ -114,6 +126,69 @@ $app->post('/admin/users/:iduser', function($iduser){
 
 	header("Location: /admin/users");
 	exit;
+});
+
+// ESQUECEU A SENHA (FORGOT)
+
+$app->get('/admin/forgot', function(){
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+});
+
+$app->post('/admin/forgot', function(){
+	
+	$user = User::getForgot($_POST['email']);
+	
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get('/admin/forgot/sent', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+});
+
+$app->get('/admin/forgot/reset', function(){
+	$user = User::validForgotDecrypt($_GET['code']);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user['desperson'],
+		"code"=>$_GET['code']
+	));
+});
+
+$app->post('/admin/forgot/reset', function(){
+	$forgot = User::validForgotDecrypt($_POST['code']);
+
+	User::setForgotUsed($forgot['idrecovery']);
+	$user = new User();
+
+	$user->get((int)$forgot['iduser']);
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
 });
 
 $app->run();
