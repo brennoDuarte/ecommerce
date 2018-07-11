77ac25985bfe5efc418016e674df11fa48a76010
Brenno Duarte <brenno.gnr@gmail.com>
2018-07-11 09:25:02 -0300

    Paginação categorias

--- admin-categories.php
+++ admin-categories.php
@@ -9,11 +9,35 @@
 
 	$app->get('/admin/categories', function(){
 		User::verifyLogin();
+
+		$search = (isset($_GET["search"])) ? $_GET["search"] : "";
+		$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
+		if ($search != "") {
+			$pagination = Category::getPageSearch($search, $page);
+		} else {
+			$pagination = Category::getPage($page);
+		}
+
+		$pages = [];
+
+		for ($i=0; $i < $pagination["pages"]; $i++) { 
+			array_push($pages, [
+				"href"=>'/admin/categories?'.http_build_query([
+					"page"=>$i+1,
+					"search"=>$search
+				]),
+				"text"=>$i+1
+			]);
+		}
+
 		$category = Category::listAll();
 		$page = new PageAdmin();
 
 		$page->setTpl("categories", [
-			'categories'=>$category
+			"categories"=>$pagination["data"],
+			"search"=>$search,
+			"pages"=>$pages
 		]);
 	});
 
